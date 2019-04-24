90ed3aa0232892f612223e83a1e3891eccef66f9
violettebec <bechoerner@gmail.com>
2019-04-24 01:29:41 -0500

    Updates

---
+++ precache-manifest.4f04b42a6ddc440ba3a5b5065024dacd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52c4f535f644a86ef8aa",
+    "url": "/ClickyGame/static/css/main.4b28e1ff.chunk.css"
+  },
+  {
+    "revision": "52c4f535f644a86ef8aa",
+    "url": "/ClickyGame/static/js/main.e9739a98.chunk.js"
+  },
+  {
+    "revision": "2c3a1b5a67c5d677a07d",
+    "url": "/ClickyGame/static/js/runtime~main.038e769c.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/ClickyGame/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "8ce53a32b0fc07901e7111068b13f294",
+    "url": "/ClickyGame/index.html"
+  }
+];
